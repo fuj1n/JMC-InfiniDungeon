2fd6d6dfcb8546f582115e9f91703b1e5950333e
fuj1n <fuj1nman@hotmail.com>
2017-08-27 13:04:33 +1000

    Made the mage quick attack spell cast after animation - Made heal audio loop better

--- Assets/Scripts/Player/MageController.cs
+++ Assets/Scripts/Player/MageController.cs
@@ -3,7 +3,7 @@
     protected override void FillSpells()
     {
         spells.Add(new SpellAttack(1, 2F, 0F).SetDamage(10F).SetPrimaryStat(PlayerData.Stats.Stat.INTELLIGENCE).SetFlySpeed(10F).SetIcon("Weapon_19").SetAnimation("Cast").SetParticle("Fire"));
-        spells.Add(new SpellAttack(5, 0F, 2F).SetDamage(5F).SetPrimaryStat(PlayerData.Stats.Stat.INTELLIGENCE).SetIcon("Weapon_20").SetAnimation("Cast").SetParticle("Fire"));
+        spells.Add(new SpellAttack(5, 0F, 2F).SetDamage(5F).SetPrimaryStat(PlayerData.Stats.Stat.INTELLIGENCE).SetFlySpeed(25F).SetIcon("Weapon_20").SetAnimation("Cast", false).SetParticle("Fire"));
         spells.Add(new SpellAttack(10, 5F, 0F).SetDamage(30F).SetPrimaryStat(PlayerData.Stats.Stat.INTELLIGENCE).SetFlySpeed(7.5F).SetIcon("Weapon_17").SetAnimation("Cast").SetParticle("Fire"));
         spells.Add(new SpellHeal(3, 5F, 30F).SetHealingPotential(.5F).SetIcon("Weapon_16").SetAnimation("Heal").SetParticle("Heal"));
     }
