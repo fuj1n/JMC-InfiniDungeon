15e4eb8b2bb13cc20980b7d92f9321055f8bb4e1
fuj1n <fuj1nman@hotmail.com>
2017-08-27 14:20:21 +1000

    Added level scaling to enemies Closes #3

---
+++ Assets/Scripts/AI/DamageScaler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageScaler : MonoBehaviour {
+
+    private float baseDamage;
+    private float baseLife;
+    private AITargetAttacker attacker;
+    private EntityLiving living;
+
+    private void Awake()
+    {
+        attacker = GetComponent<AITargetAttacker>();
+        living = GetComponent<EntityLiving>();
+        baseDamage = attacker.damage;
+        baseLife = living.maxLife;
+    }
+
+    private void Update()
+    {
+        if (PlayerData.Instance == null)
+            return;
+
+        attacker.damage = baseDamage * (PlayerData.Instance.level * .15F + 1);
+        living.maxLife = baseLife * PlayerData.Instance.level;
+    }
+}
